8000cfb7f6168b2b962ebc01465b1be90c1dc859
Yashashvi Singh <your-github-email@gmail.com>
2026-06-10 13:24:54 +0530

    Implemented customer dashboard UI

--- frontend/src/app/features/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { DashboardCard } from '../../shared/components/dashboard-card/dashboard-card';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [DashboardCard],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
 export class Dashboard {
+
+  totalBookings = 12;
+  completedServices = 8;
+  inProgress = 2;
+  readyForDelivery = 2;
+
+  recentBookings = [
+    {
+      bookingId: 101,
+      vehicle: 'Honda City',
+      service: 'Premium Service',
+      status: 'Repair'
+    },
+    {
+      bookingId: 102,
+      vehicle: 'Hyundai i20',
+      service: 'Basic Service',
+      status: 'Ready For Delivery'
+    },
+    {
+      bookingId: 103,
+      vehicle: 'Tata Nexon',
+      service: 'AC Service',
+      status: 'Inspection'
+    }
+  ];
 
 }
--- frontend/src/app/shared/components/dashboard-card/dashboard-card.ts
+++ frontend/src/app/shared/components/dashboard-card/dashboard-card.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-card',
+  standalone: true,
   imports: [],
   templateUrl: './dashboard-card.html',
-  styleUrl: './dashboard-card.css',
+  styleUrl: './dashboard-card.css'
 })
 export class DashboardCard {
+
+  @Input() title: string = '';
+  @Input() count: number = 0;
 
 }
