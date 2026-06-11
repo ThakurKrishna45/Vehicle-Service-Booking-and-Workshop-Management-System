de1f73d5f0748a24e4f7264346b4be2684f665ad
ThakurKrishna45 <215252541+ThakurKrishna45@users.noreply.github.com>
2026-06-11 21:41:03 +0530

    Added dummy data

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'services',
+    loadComponent: () =>
+      import('./features/service-catalog/service-list/service-list')
+      .then(m => m.ServiceListComponent)
+  },
+
+  {
+    path: 'services/:id',
+    loadComponent: () =>
+      import('./features/service-catalog/service-details/service-details')
+      .then(m => m.ServiceDetailsComponent)
+  },
+
+{
+  path: 'booking',
+  loadComponent: () =>
+    import('./features/booking/create-booking/create-booking')
+      .then(m => m.CreateBookingComponent)
+},
+  {
+    path: 'bookings',
+    loadComponent: () =>
+      import('./features/booking/booking-history/booking-history')
+      .then(m => m.BookingHistoryComponent)
+  }
+
+];
