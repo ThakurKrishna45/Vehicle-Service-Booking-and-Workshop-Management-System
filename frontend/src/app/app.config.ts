50d7518880f569e7e611c889f0c67a263b3cb09b
ThakurKrishna45 <215252541+ThakurKrishna45@users.noreply.github.com>
2026-06-11 21:57:18 +0530

    solved merge conflicts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,47 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners
-} from '@angular/core';
-
-import {
-  provideRouter
-} from '@angular/router';
-
-import {
-  provideHttpClient
-} from '@angular/common/http';
-
-import {
-  provideStore
-} from '@ngrx/store';
+import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
-
-import {
-  filterReducer
-} from './store/service-filter/service-filter.reducer';
+import { filterReducer } from './store/service-filter/service-filter.reducer';
 
 export const appConfig: ApplicationConfig = {
-
   providers: [
-
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
-    provideRouter(routes)
-
     provideRouter(routes),
-
-    provideHttpClient(),
-
     provideStore({
-
       filters: filterReducer
-
     })
-
   ]
-
 };
